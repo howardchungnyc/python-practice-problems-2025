2f9b27be38a42924518b41e01f2a4042e50433fa
Howard Chung <146138513+howardchungnyc@users.noreply.github.com>
2025-09-17 11:33:49 -0400

    problem 1 answered

--- problems/problem_001.py
+++ problems/problem_001.py
@@ -8,5 +8,32 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+# Input:
+# data type: integers
+
+# Output
+# data type: integers
+
+# Examples
+# value1 = 2
+# value2 = 3
+
+# Conditions (if)
+
+# Iteration (loop)
+
 def minimum_value(value1, value2):
-    pass
+    if value1 == value2:
+        return value1
+    else:
+        return min(value1, value2)
+
+value1 = 2
+value2 = 3
+value1_two = 1
+value2_two = 1
+
+result = minimum_value(value1, value2)
+result2 = minimum_value(value1_two, value2_two)
+print(f"result is: {result}")
+print(f"result2 is: {result2}")
