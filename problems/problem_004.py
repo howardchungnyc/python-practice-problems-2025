d13b1bbf5c55eecb00cc242d4a89a314bc78737b
Howard Chung <146138513+howardchungnyc@users.noreply.github.com>
2025-09-18 08:56:52 -0400

    finished prob 4: return max val given 3 vals

--- problems/problem_004.py
+++ problems/problem_004.py
@@ -12,5 +12,45 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+# Inputs: three values
+# Outputs:
+# max value among three
+# if two values are same, return either val
+# if three vals are same, return any val
+
+
 def max_of_three(value1, value2, value3):
-    pass
+    if value1 == value2 and value2 == value3:
+        return value1
+    elif value1 == value2 or value1 == value3:
+        return value1
+    elif value2 == value3:
+        return value2
+    else:
+        return max(value1, value2, value3)
+
+
+# Sample Inputs:
+value1 = 1
+value2 = 2
+value3 = 3
+
+value1_two_same = 4
+value2_two_same = 4
+value3_two_same = 5
+
+value1_three_same = 6
+value2_three_same = 6
+value3_three_same = 6
+
+# Function Invocation:
+result_max = max_of_three(value1, value2, value3)
+print(f"result_max: {result_max}")
+
+result_two_same = max_of_three(value1_two_same, value2_two_same, value3_two_same)
+print(f"result_two_same: {result_two_same}")
+
+result_three_same = max_of_three(
+    value1_three_same, value2_three_same, value3_three_same
+)
+print(f"result_three_same: {result_three_same}")
